31d4bf869da4f5853895ada0814f0c338147e1ec
Ankur Vadhwani <www.nankur113@gmail.com>
2020-12-02 21:45:03 +0530

    updated

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Button } from "@material-ui/core";
 import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
+import BookmarkBorderOutlinedIcon from "@material-ui/icons/BookmarkBorderOutlined";
 
 function Note(props) {
   return (
     <div className="note">
+      <BookmarkBorderOutlinedIcon className="markicon" />
       <h1>{props.title}</h1>
       <p>{props.content}</p>
       <Button
